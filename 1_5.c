4153b229989f4de8d39411773ce77c40dec48d60
TShibano <46261399+TShibano@users.noreply.github.com>
2019-10-02 12:24:38 +0900

    Update 1_5.c

--- 1_5.c
+++ 1_5.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-#include <gconio.h>
+#include <conio.h>
 
 int main(void){
-    int cnt = 0;
+    /* int cnt = 0; */
     while (1){
         if (_kbhit() != 0){
-            char ans = getch();
-            printf("%s", ans);
+            char ans = _getch();
+         /* char ans = _getche(); */
+         printf("%c", ans);
+            break;
         }
 
         printf("Waiting for keyin.\n");
