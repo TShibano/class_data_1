199e7de40e74969ba221cc79b9711c2442d58ca4
TShibano <46261399+TShibano@users.noreply.github.com>
2019-10-08 13:27:35 +0900

    teisei10/6

--- 1_6.c
+++ 1_6.c
@@ -1,25 +1,26 @@
+#define _USE_MATH_DEFINES
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
-#define _USE_MATH_DEFINES
 
 int main(void){
     int a = 2;
     double b = 8;
-    printf("2^8 = %lf\n", pow(a, b));
+    int ans = int(pow(a, b));
+    printf("2^8 = %d\n", ans);
 
 
     double alpha = 67; // 角度(単位は°)
     double theta = M_PI * 67 / 180; // radに変換
-    printf("sin(67°) = %f\n", sin(theta));
+    printf("sin(67°) = %lf\n", sin(theta));
 
     double c = -23.456;
-    printf("|-23.456| = %f\n", fabs(c));
+    printf("|-23.456| = %.3lf\n", fabs(c));
 
     double d = 7;
     double e = 3;
-    printf("7/3の剰余 =  %lf\n", int(fmod(d, e)));
+    printf("7/3の剰余 =  %d\n", int(fmod(d, e)));
 
     srand((unsigned int) time(NULL));   // 現在時刻をもとにseedを生成
     for (int i = 0; i < 10; i++){
