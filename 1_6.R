7669e15a29f117379f2cd43f10546f8e024a6688
TShibano <46261399+TShibano@users.noreply.github.com>
2019-10-15 18:47:55 +0900

    OK

---
+++ 1_6.R
@@ -0,0 +1,22 @@
+# 1
+a = 2
+b = 8
+print(a^b)
+
+# 2
+do = pi/180
+c = 67
+print(sin(c * do))
+
+# 3
+d = -23.456
+print(abs(d))
+
+# 4
+e = 7
+f = 3
+print(e%%f)
+
+# 5
+set.seed(0)
+runif(10, min = 0, max = 1)
---
+++ 1_6.py
@@ -0,0 +1,26 @@
+import math
+import random
+
+# 1
+a = 2
+b = 8
+print(pow(2, 8))
+
+# 2
+c = 67
+print(math.sin(math.radians(c)))
+
+# 3
+d = -23.456
+print(abs(d))
+
+# 4
+e = 7
+f = 3
+print(e % f)
+
+# 5
+random.seed(0)
+for i in range(10):
+    print(random.uniform(0, 1))
+
