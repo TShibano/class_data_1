b32b0a46772638509584ed578ec06b739bc43ccc
TShibano <46261399+TShibano@users.noreply.github.com>
2019-11-22 16:59:00 +0900

    2019/11/22

--- 2_1.c
+++ 2_1.c
@@ -2,21 +2,21 @@
 #include <math.h>
 #define _USE_MATH_DEFINES
 
+
 int main(void){
     double y[11];
     double A = 50;
     double T = 4;
     double lambda = 10;
-    double time[11];
-
+    //double time[11];
+    double t = 5;
     for (int i = 0; i <= 10; i++){
-        scanf("%lf", &time[i]);
         int x = 2 * i;
-        y[i] = A * sin(2 * M_PI * (time[i] / T - x / lambda));
+        y[i] = A * sin(2 * M_PI * (t / T - x / lambda));
     }
 
     for (int i = 0; i <= 10; i++){
-        printf("%d:time is %lf: %lf\n", i, time[i], y[i]);
+        printf("%d: %lf\n", 2 * i, y[i]);
     }
 
     return 0;
